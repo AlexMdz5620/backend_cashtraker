3a0a7c4df0d74a6c8386c8a0bb34dab34e0f3cf7
Manu Mdz <manuel.mendozac.dev@gmail.com>
2025-08-07 10:27:18 -0600

    Test - Auth Users

--- src/tests/integration/app.test.ts
+++ src/tests/integration/app.test.ts
@@ -1,6 +1,9 @@
 import request from "supertest";
 import server from "../../server";
 import { AuthController } from "../../controllers/AuthController";
+import User from "../../models/User";
+import * as authUtils from "../../utils/auth";
+import * as jwtUtils from '../../utils/jwt';
 
 describe('Auth - Create Account', () => {
     it('should display validation errors when form is empty', async () => {
@@ -124,9 +127,173 @@ describe('Auth - Account Confirmation with Token or not valid', () => {
         const response = await request(server)
             .post('/api/auth/confrim-account')
             .send({ token });
-            
+
         expect(response.status).toBe(200);
         expect(response.body).toEqual('Cuenta confirmada correctametne');
         expect(response.status).not.toBe(400);
+    });
+});
+
+describe('Auth - Login', () => {
+    beforeEach(() => {
+        jest.clearAllMocks();
+    });
+
+    it('should display validation errors when the from is empty', async () => {
+        const response = await request(server)
+            .post('/api/auth/login')
+            .send({});
+
+        const loginMock = jest.spyOn(AuthController, 'login');
+
+        expect(response.status).toBe(400);
+        expect(response.body).toHaveProperty('errors');
+        expect(response.body.errors).toHaveLength(2);
+
+        expect(response.body.errors).not.toHaveLength(1);
+        expect(loginMock).not.toHaveBeenCalled();
+    });
+
+    it('should return 400 bad request when the email is invalid', async () => {
+        const response = await request(server)
+            .post('/api/auth/login')
+            .send({
+                password: "password",
+                email: "not-valid"
+            });
+
+        const loginMock = jest.spyOn(AuthController, 'login');
+
+        expect(response.status).toBe(400);
+        expect(response.body).toHaveProperty('errors');
+        expect(response.body.errors).toHaveLength(1);
+        expect(response.body.errors[0].msg).toBe('E-mail no válido');
+
+        expect(response.body.errors).not.toHaveLength(2);
+        expect(loginMock).not.toHaveBeenCalled();
+    });
+
+    it('should return 404 error if the user not found', async () => {
+        const response = await request(server)
+            .post('/api/auth/login')
+            .send({
+                password: "password",
+                email: "user_not_found@test.com"
+            });
+
+        expect(response.status).toBe(404);
+        expect(response.body).toHaveProperty('error');
+        expect(response.body.error).toBe('Usuario no encontrado');
+        expect(response.status).not.toBe(200);
+    });
+
+    it('should return 403 error if the user account is not confirmed', async () => {
+        (jest.spyOn(User, 'findOne') as jest.Mock)
+            .mockResolvedValue({
+                id: 1,
+                confirm: false,
+                password: 'hashed_password',
+                email: "user_not_confirmed@test.com"
+            });
+
+        const response = await request(server)
+            .post('/api/auth/login')
+            .send({
+                password: "password",
+                email: "user_not_confirmed@test.com"
+            });
+
+        expect(response.status).toBe(403);
+        expect(response.body).toHaveProperty('error');
+        expect(response.body.error).toBe('La cuenta no a sido confirmada');
+        expect(response.status).not.toBe(200);
+        expect(response.status).not.toBe(404);
+    });
+
+    it('should return 403 error if the user account is not confirmed', async () => {
+        const userData = {
+            name: "Test",
+            password: "password",
+            email: "user_not_confirmed@test.com",
+        }
+
+        await request(server)
+            .post('/api/auth/create-account')
+            .send(userData);
+
+        const response = await request(server)
+            .post('/api/auth/login')
+            .send({
+                password: userData.password,
+                email: userData.email
+            });
+
+        expect(response.status).toBe(403);
+        expect(response.body).toHaveProperty('error');
+        expect(response.body.error).toBe('La cuenta no a sido confirmada');
+        expect(response.status).not.toBe(200);
+        expect(response.status).not.toBe(404);
+    });
+
+    it('should return 401 error if the password is incorrect', async () => {
+        const findOne = (jest.spyOn(User, 'findOne') as jest.Mock)
+            .mockResolvedValue({
+                id: 1,
+                confirm: true,
+                password: 'hashed_password',
+            });
+
+        const checkPass = jest.spyOn(authUtils, 'checkPass').mockResolvedValue(false);
+
+        const response = await request(server)
+            .post('/api/auth/login')
+            .send({
+                password: "wrongPassword",
+                email: "test@test.com"
+            });
+
+        expect(response.status).toBe(401);
+        expect(response.body).toHaveProperty('error');
+        expect(response.body.error).toBe('Password incorrecto');
+
+        expect(response.status).not.toBe(200);
+        expect(response.status).not.toBe(404);
+        expect(response.status).not.toBe(403);
+
+        expect(findOne).toHaveBeenCalledTimes(1);
+        expect(checkPass).toHaveBeenCalledTimes(1);
+    });
+
+    it('should return 401 error if the password is incorrect', async () => {
+        const findOne = (jest.spyOn(User, 'findOne') as jest.Mock)
+            .mockResolvedValue({
+                id: 1,
+                confirm: true,
+                password: 'hashed_password',
+            });
+
+        const checkPass = jest.spyOn(authUtils, 'checkPass').mockResolvedValue(true);
+        const generateJWT = jest.spyOn(jwtUtils, 'generateJWT').mockReturnValue('jwt_token');
+
+        const response = await request(server)
+            .post('/api/auth/login')
+            .send({
+                password: "correctPassword",
+                email: "test@test.com"
+            });
+
+        expect(response.status).toBe(200);
+        expect(response.body).toEqual('jwt_token');
+
+        expect(findOne).toHaveBeenCalled();
+        expect(findOne).toHaveBeenCalledTimes(1);
+
+        expect(checkPass).toHaveBeenCalled();
+        expect(checkPass).toHaveBeenCalledTimes(1);
+        expect(checkPass).toHaveBeenCalledWith('correctPassword', 'hashed_password');
+        
+        expect(generateJWT).toHaveBeenCalled();
+        expect(generateJWT).toHaveBeenCalledTimes(1);
+        expect(checkPass).toHaveBeenCalledWith(1);
     });
 });
